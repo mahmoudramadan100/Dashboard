b57e0fb6e01ee16c2e60f036882af9db2f1d1436
mahmoud ramadan <mahmoudr03@gmail.com>
2022-12-28 13:25:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// const toggleSidebar = () => document.body.classList.toggle("open");
+let btn = document.querySelector('.header');
+let menu = document.querySelector('.navbar');
+let nav = document.querySelector('.nav');
+let body = document.querySelector('.dashboard');
+let close = document.querySelector('.close');
+let open = document.querySelector('.open-nav');
+
+// Slider
+var swiper = new Swiper(".imgSlider", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    mousewheel: true,
+    keyboard: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    freeMode: true,
+});
+
+
+
+
+
+ btn.onclick = function() {
+    // nav.classList.remove('close-nav');
+   nav.classList.toggle('open-nav');
+//    nav.classList.add('width');
+//    nav.classList.add('transition');
+   nav.style.width = "75px";
+ }
